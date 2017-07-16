0da6a72d8d27d5d7edfd4f784eb40f7c3e1dc918
cuiqiusheng <363092524@qq.com>
2017-07-16 22:23:29 +0800

    font size

--- public/fontSize.js
+++ public/fontSize.js
@@ -1,38 +1,7 @@
-/* eslint-disable */
-! function (e) {
-  function t(a) {
-    if (i[a]) return i[a].exports;
-    var n = i[a] = {
-      exports: {},
-      id: a,
-      loaded: !1
-    };
-    return e[a].call(n.exports, n, n.exports, t), n.loaded = !0, n.exports
+; (function (win, doc) {
+  function change() {
+    doc.documentElement.style.fontSize = 100 * doc.documentElement.clientWidth / 640 + 'px';
   }
-  var i = {};
-  return t.m = e, t.c = i, t.p = "", t(0)
-}([function (e, t) {
-  "use strict";
-  Object.defineProperty(t, "__esModule", {
-    value: !0
-  });
-  var i = window;
-  t["default"] = i.flex = function (e, t) {
-    var a = e || 100,
-      n = t || 1,
-      r = i.document,
-      o = navigator.userAgent,
-      d = o.match(/Android[\S\s] AppleWebkit\/(\d{3})/i),
-      l = o.match(/U3\/((\d |\.){5,})/i),
-      c = l && parseInt(l[1].split(".").join(""), 10) >= 80,
-      p = navigator.appVersion.match(/(iphone|ipad|ipod)/gi),
-      s = i.devicePixelRatio || 1;
-    p || d && d[1] > 534 || c || (s = 1);
-    var u = 1 / s,
-      m = r.querySelector('meta[name="viewport"]');
-    m || (m = r.createElement("meta"), m.setAttribute("name", "viewport"), r.head.appendChild(m)), m.setAttribute(
-      "content", "width=device-width,user-scalable=no,initial-scale=", ",maximum-scale=", ",minimum-scale=", ),
-      r.documentElement.style.fontSize = a / 2 * s * n + "px"
-  }, e.exports = t["default"]
-}]);
-flex(100, 1);
+  change();
+  win.addEventListener('resize', change, false);
+})(window, document);
